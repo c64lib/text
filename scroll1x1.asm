6ff71fba5afe03a47243ef00667b515f0340722b
Maciej Małecki <malecki.maciej@gmail.com>
2018-08-09 23:23:30 +0200

    Txt 3 (#4)
    Implementation of scroll 1x1

---
+++ scroll1x1.asm
@@ -0,0 +1,59 @@
+#import "common/invoke.asm"
+#import "common/mem.asm"
+#importonce
+.filenamespace c64lib
+
+/*
+ * Stack parameters (order of pushing):
+ * screenAddress PTR
+ * textAddress PTR
+ * textPtr WORD
+ */
+.macro @scroll1x1(tempZero1) {
+  .assert "tempZero1 must be a zero page address", tempZero1 <= 255, true
+  .print "tempZero1 installed at " + toHexString(tempZero1) + " and " + toHexString(tempZero1 + 1)
+
+  invokeStackBegin(returnPtr)
+  pullParamW(scrollPtr)
+  pullParamW(textPtr)
+  pullParamW(screenPtr)
+  
+  // shift text to left
+  copyFast(screenPtr, tempZero1, 2)
+  ldy #$00
+shiftText:
+  iny
+  lda (tempZero1), y
+  dey
+  sta (tempZero1), y
+  iny
+  cpy #39
+  bne shiftText
+  
+  // place next char
+  copyFast(scrollPtr, tempZero1, 2)
+  ldy #$00
+  lda (tempZero1), y
+  cmp #$ff
+  bne placeChar
+  copyFast(textPtr, scrollPtr, 2)
+  copyFast(scrollPtr, tempZero1, 2)
+  lda (tempZero1), y
+placeChar:
+  sta lastChar
+  add16(39, screenPtr)
+  copyFast(screenPtr, tempZero1, 2)
+  lda lastChar
+  ldy #$00
+  sta (tempZero1), y
+  add16(1, scrollPtr)
+  pushParamWInd(scrollPtr)
+  invokeStackEnd(returnPtr)
+  rts
+  // local variables
+  returnPtr: .word $ffff
+  scrollPtr: .word $ffff
+  textPtr:   .word $ffff
+  screenPtr: .word $ffff
+  lastChar:  .byte $ff
+}
