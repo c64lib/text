a1dcd2628b9a930a5bcc82cc7ddced1e9ea0fcca
Maciej Malecki <malecki.maciej@gmail.com>
2019-01-13 00:26:14 +0100

    Working on tiles 2x2

---
+++ lib/tiles2.asm
@@ -0,0 +1,17 @@
+/*
+ * c64lib/text/tiles2.asm
+ *
+ * A library that supports drawing and 4-directional scrolling of 2x2 tile map using text modes
+ * of VIC-2. All three text modes are supported.
+ *
+ * Color limitations:
+ *  - color RAM is configured per 2x2 tile
+ *
+ * Author:    Maciej Malecki
+ * License:   MIT
+ * (c):       2019
+ * GIT repo:  https://github.com/c64lib/text
+ */
+#importonce
+.filenamespace c64lib
+
