f760fa2fd63b5de40a5e49a9b3bc2f4dca6acb8a
Maciej Malecki <malecki.maciej@gmail.com>
2019-01-16 23:29:54 +0100

    Working on tile 2 ongoing

--- lib/tiles2.asm
+++ lib/tiles2.asm
@@ -15,6 +15,8 @@
 #importonce
 .filenamespace c64lib
 
+// ==== Public declarations ====
+
 /*
  * Config record for 2x2 tile scrollable playfield.
  */
@@ -23,33 +25,77 @@
   startRow,
   // end text row of the tiled playfield, values 1..24
   endRow,
-  // address (16 bit) of page 0 (1024 bytes) for double buffering
+  // vic 2 bank 0..3
+  bank,
+  // page number (0..15) of page 0 (1024 bytes) for double buffering
   page0,
-  // address (16 bit) of page 1 (1024 bytes) for double buffering
+  // page number (0..15) of page 1 (1024 bytes) for double buffering
   page1,
-  // address (16 bit) of playfield width in tiles (1 byte)
+  // address (8 or 16 bit) of playfield width in tiles (1 byte)
   width,
-  // address (16 bit) of playfield height (1 byte)
+  // address (8 or 16 bit) of playfield height (1 byte)
   height,
-  // address (16 bit) for tile definitions (1kb)
+  // address (8 or 16 bit) for tile definitions (1kb)
   tileDefinition,
-  // address (16 bit) for tile colors (256b)
+  // address (8 or 16 bit) for tile colors (256b)
   tileColors,
-  // address (16 bit) for map definition (width * height) bytes
+  // address (8 or 16 bit) for map definition (width * height) bytes
   mapDefinition,
-  // address (16 bit) for X position of top left corner (1b)
+  // address (8 or 16 bit) for X position of top left corner (2b), 1st byte - tile position, 2nd byte - sub tile position
   x,
-  // address (16 bit) for Y position of top left corner (1b)
+  // address (8 or 16 bit) for Y position of top left corner (2b), 1st byte - tile position, 2nd byte - sub tile position
   y,
-  // address (16 bit) for display phase counter
-  phase
+  // address (8 or 16 bit) for display phase counter
+  phase,
+  // ---- zero page mandatory variables
+  // general purpose zero page accumulator for indirect addressing
+  addrAccumulator
 }
 
-.macro drawTile2(tile2Config) {
+// ==== Public hosted subroutines ====
+
+.macro tile2Init(cfg) {
+  _t2_validate(cfg)
+  lda #$00
+  sta cfg.phase
+}
+
+.macro tile2Draw(cfg) {
+  _t2_validate(cfg)
+}
+
+.macro tile2Animate(cfg) {
+  _t2_validate(cfg)
+}
+
+// ==== Private stuff ====
+
+.macro _t2_shiftScreenLeft(cfg) {
+}
+
+.macro _t2_validate(tile2Config) {
   .assert "startRow must be smaller than endRow", tile2Config.startRow < tile2Config.endRow, true
+  .assert "addrAccumulator must be defined on zero page", tile2Config.addrAccumulator < 256, true
 }
 
+.function _t2_screenAddress(cfg, page) {
+  .return (cfg.bank*16 + _t2_pageToPageNumber(cfg, page)) * 1024
+}
+
+.function _t2_pageToPageNumber(cfg, page) {
+  .if (page == 0) {
+    .return cfg.page0;
+  } else {
+    .return cfg.page1
+  }
+}
+
+// to be removed
 .var cfg = Tile2Config()
 .eval cfg.startRow = 0
 .eval cfg.endRow = 24
-drawTile2(cfg)
+.eval cfg.phase = $2
+.eval cfg.addrAccumulator = $3
+tile2Init(cfg)
+tile2Draw(cfg)
+tile2Animate(cfg)
