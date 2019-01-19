cca81e0d27ecd736f9cd844862a20d1dd0917c2f
Maciej Malecki <malecki.maciej@gmail.com>
2019-01-20 00:22:18 +0100

    spec for shift right

--- spec/tiles2.spec.asm
+++ spec/tiles2.spec.asm
@@ -7,7 +7,15 @@ sfspec: init_spec()
   it("shifts screen to the left by 1 byte"); {
     jsr shiftScreenLeft
     
-    assert_bytes_equal 1000: testScreenData_left: expectedScreenData_left
+    assert_bytes_equal 1000: testScreenData: expectedScreenData_left
+  }
+  
+  describe("_t2_shiftScreenRight")
+  
+  it("shifts screen to the right by 1 byte"); {
+    jsr shiftScreenRight
+    
+    assert_bytes_equal 1000: testScreenData: expectedScreenData_right
   }
 
 finish_spec()
@@ -16,15 +24,16 @@ finish_spec()
 .namespace c64lib {
 .var @cfg = Tile2Config()
 .eval @cfg.bank = 0
-.eval @cfg.page0 = 4096/1024
-.eval @cfg.startRow = 3
-.eval @cfg.endRow = 20
+.eval @cfg.page0 = 5120/1024
+.eval @cfg.startRow = 1
+.eval @cfg.endRow = 23
 }
 
 shiftScreenLeft: .namespace c64lib { _t2_shiftScreenLeft(@cfg, 0); rts }
+shiftScreenRight: .namespace c64lib { _t2_shiftScreenRight(@cfg, 0); rts }
 
 .align $400
-testScreenData_left: {
+testScreenData: {
   .for(var y = 0; y < cfg.startRow; y++) {
     .fill 40, i
   }
@@ -37,7 +46,7 @@ testScreenData_left: {
     .fill 40, i
   }
 }
-.print testScreenData_left
+.print "test data address = " + testScreenData
 
 expectedScreenData_left: {
   .for(var y = 0; y < cfg.startRow; y++) {
@@ -47,6 +56,20 @@ expectedScreenData_left: {
   .for(var y = cfg.startRow; y <= cfg.endRow; y++) {
     .fill 39, <(v + i + 1)
     .byte <(v + 39)
+    .eval v++
+  }
+  .for(var y = cfg.endRow + 1; y < 25; y++) {
+    .fill 40, i
+  }
+}
+expectedScreenData_right: {
+  .for(var y = 0; y < cfg.startRow; y++) {
+    .fill 40, i
+  }
+  .var v = 0
+  .for(var y = cfg.startRow; y <= cfg.endRow; y++) {
+    .byte <(v + 1)
+    .fill 39, <(v + i + 1)
     .eval v++
   }
   .for(var y = cfg.endRow + 1; y < 25; y++) {
